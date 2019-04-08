77f576605589f45d74faecc2a281546904e00e82
vsmid <vedransmid@yeti-it.hr>
2019-04-08 19:00:50 +0200

    Added basic CORS filter.

---
+++ rudimentary-server/src/main/java/hr/yeti/rudimentary/server/security/cors/CorsFilter.java
@@ -0,0 +1,39 @@
+package hr.yeti.rudimentary.server.security.cors;
+
+import com.sun.net.httpserver.HttpExchange;
+import hr.yeti.rudimentary.config.ConfigProperty;
+import hr.yeti.rudimentary.http.filter.spi.HttpFilter;
+import java.io.IOException;
+
+public class CorsFilter extends HttpFilter {
+
+  private ConfigProperty corsEnabled = new ConfigProperty("security.cors.enabled");
+  private ConfigProperty corsAllowOrigin = new ConfigProperty("security.cors.allowOrigin");
+  private ConfigProperty corsAllowHeaders = new ConfigProperty("security.cors.allowHeaders");
+  private ConfigProperty corsExposeHeaders = new ConfigProperty("security.cors.exposeHeaders");
+  private ConfigProperty corsAllowCredentials = new ConfigProperty("security.cors.allowCredentials");
+  private ConfigProperty corsAllowMethods = new ConfigProperty("security.cors.allowMethods");
+  private ConfigProperty corsMaxAge = new ConfigProperty("security.cors.maxAge");
+
+  @Override
+  public boolean activatingCondition() {
+    return corsEnabled.asBoolean();
+  }
+
+  @Override
+  public void doFilter(HttpExchange exchange, Chain chain) throws IOException {
+    exchange.getResponseHeaders().add("Access-Control-Allow-Origin", corsAllowOrigin.value());
+    exchange.getResponseHeaders().add("Access-Control-Allow-Headers", corsAllowHeaders.value());
+    exchange.getResponseHeaders().add("Access-Control-Expose-Headers", corsExposeHeaders.value());
+    exchange.getResponseHeaders().add("Access-Control-Allow-Credentials", corsAllowCredentials.value());
+    exchange.getResponseHeaders().add("Access-Control-Allow-Methods", corsAllowMethods.value());
+    exchange.getResponseHeaders().add("Access-Control-Max-Age", corsMaxAge.value());
+    chain.doFilter(exchange);
+  }
+
+  @Override
+  public String description() {
+    return "Filter which enables cross origin resource sharing.";
+  }
+
+}
