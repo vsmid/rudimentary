55ef5e57edf3652289d9359e1472a61c8c75ccdd
vsmid <vedransmid@yeti-it.hr>
2019-04-20 10:54:31 +0200

    Added load sql script to demo DbInitializer.

--- rudimentary-demo/src/main/java/hr/yeti/rudimentary/demo/DbInitializer.java
+++ rudimentary-demo/src/main/java/hr/yeti/rudimentary/demo/DbInitializer.java
@@ -3,6 +3,8 @@ package hr.yeti.rudimentary.demo;
 import hr.yeti.rudimentary.context.spi.Instance;
 import hr.yeti.rudimentary.sql.Sql;
 import hr.yeti.rudimentary.sql.spi.JdbcConnectionPool;
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class DbInitializer implements Instance {
 
@@ -20,4 +22,30 @@ public class DbInitializer implements Instance {
     return new Class[]{ JdbcConnectionPool.class };
   }
 
+  /**
+   * Loads script from classpath.
+   *
+   * @param script
+   */
+  private void loadScript(InputStream script) {
+    Scanner scanner = new Scanner(script);
+    scanner.useDelimiter("(;(\r)?\n)|(--\n)");
+
+    Sql.tx((sql) -> {
+      while (scanner.hasNext()) {
+        String line = scanner.next();
+        if (line.startsWith("/*!") && line.endsWith("*/")) {
+          int i = line.indexOf(' ');
+          line = line.substring(i + 1, line.length() - " */".length());
+        }
+        if (line.trim().length() > 0) {
+          if (!line.endsWith(";")) {
+            line += ";";
+          }
+          sql.update(line);
+        }
+      }
+      return null;
+    });
+  }
 }
