43dbe34760a0cd728743c45de28c94263270ac58
vsmid <vedransmid@yeti-it.hr>
2019-07-02 21:19:20 +0200

    Better ContextMock initialization.

--- rudimentary-api/src/main/java/hr/yeti/rudimentary/test/ContextMock.java
+++ rudimentary-api/src/main/java/hr/yeti/rudimentary/test/ContextMock.java
@@ -3,29 +3,47 @@ package hr.yeti.rudimentary.test;
 import hr.yeti.rudimentary.config.spi.Config;
 import hr.yeti.rudimentary.context.spi.Context;
 import hr.yeti.rudimentary.context.spi.Instance;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public final class ContextMock extends Context {
 
+  private List<Instance> instances = new ArrayList<>();
+
   public ContextMock(Instance... instances) {
-    this.setup(null, instances);
+    this.instances.addAll(Arrays.asList(instances));
+    initialize();
   }
 
-  public ContextMock(Config config, Instance... instances) {
-    this.setup(config, instances);
-  }
-
-  private void setup(Config config, Instance... instances) {
-    // Used to test destroy method.
-    super.destroy();
+  public ContextMock(Map<String, String> properties, Class<? extends Instance>... instances) {
+    Config config = new ConfigMock().load(properties);
+    config.seal();
 
     if (Objects.nonNull(config)) {
       super.initializeInstance(config);
       super.add(config);
     }
 
+    for (Class<? extends Instance> instance : instances) {
+      try {
+        this.instances.add(instance.getDeclaredConstructor().newInstance());
+      } catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+        Logger.getLogger(ContextMock.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
+
+    initialize();
+  }
+
+  @Override
+  public void initialize() {
+    super.destroy();
     for (Instance instance : instances) {
       super.add(instance);
     }
@@ -39,7 +57,6 @@ public final class ContextMock extends Context {
         .forEach((instance) -> {
           super.initializeInstance(instance);
         });
-
   }
 
   // Method used to test protected Context#isInstanceInitialized method.
--- rudimentary-server/src/test/java/hr/yeti/rudimentary/server/crypto/Pbkdf2Test.java
+++ rudimentary-server/src/test/java/hr/yeti/rudimentary/server/crypto/Pbkdf2Test.java
@@ -1,7 +1,5 @@
 package hr.yeti.rudimentary.server.crypto;
 
-import hr.yeti.rudimentary.server.ContextMock;
-import hr.yeti.rudimentary.test.ConfigMock;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -10,8 +8,6 @@ public class Pbkdf2Test {
   @Test
   public void test_password_generation() {
     // setup:
-    ContextMock contextMock = new ContextMock(new ConfigMock());
-
     Pbkdf2 pbkdf2 = new Pbkdf2();
     String hashedPassword;
 
