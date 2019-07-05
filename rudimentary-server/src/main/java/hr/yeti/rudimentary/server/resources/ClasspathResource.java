7a059c91cefe54e310a56208fa3fbc78e40264dd
Vedran Smid <vedransmid@yeti-it.hr>
2019-07-05 21:24:37 +0200

    Added ClasspathResource for convenient resource loading.

---
+++ rudimentary-server/src/main/java/hr/yeti/rudimentary/server/resources/ClasspathResource.java
@@ -0,0 +1,21 @@
+package hr.yeti.rudimentary.server.resources;
+
+import java.io.InputStream;
+import java.util.Objects;
+
+public class ClasspathResource {
+
+  private String path;
+
+  public ClasspathResource(String path) {
+    Objects.requireNonNull(path);
+
+    this.path = !path.startsWith("/") ? "/" + path : path;
+  }
+
+  public InputStream get() {
+    return this.getClass().
+        getResourceAsStream(path);
+  }
+
+}
