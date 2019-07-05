4ef130762a61ed518498be3270939b6eee59b46a
Vedran Smid <vedransmid@yeti-it.hr>
2019-07-05 21:25:03 +0200

    Added ClasspathResourceTest.

---
+++ rudimentary-server/src/test/java/hr/yeti/rudimentary/server/resources/ClasspathResourceTest.java
@@ -0,0 +1,16 @@
+package hr.yeti.rudimentary.server.resources;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ClasspathResourceTest {
+
+  @ParameterizedTest
+  @ValueSource(strings = { "/templates/apidocs.html", "templates/apidocs.html" })
+  public void test_get_resource(String path) {
+    expect:
+    assertNotNull(new ClasspathResource(path).get());
+  }
+
+}
