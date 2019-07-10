dbb63f6a5827a1678c0702fae8c61e62469239f9
Vedran Smid <vedransmid@yeti-it.hr>
2019-07-10 20:11:42 +0200

    Added new module, a Maven plugin to auto register service providers. For now it is a working example supporting only HttpEndpoint, ViewEndpoint and Instance providers.

---
+++ rudimentary-autoregister-maven-plugin/src/main/java/hr/yeti/rudimentary/autoregister/maven/plugin/AutoRegisterMojo.java
@@ -0,0 +1,29 @@
+package hr.yeti.rudimentary.autoregister.maven.plugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "auto-register")
+public class AutoRegisterMojo extends AbstractMojo {
+
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    Path projectDir = new File("").toPath();
+    Path sourcesDir = projectDir.resolve("src").resolve("main").resolve("java");
+
+    try {
+      Files.walkFileTree(sourcesDir, new RegisterAsServiceProvider());
+    } catch (IOException ex) {
+      Logger.getLogger(AutoRegisterMojo.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+}
---
+++ rudimentary-autoregister-maven-plugin/src/main/java/hr/yeti/rudimentary/autoregister/maven/plugin/RegisterAsServiceProvider.java
@@ -0,0 +1,96 @@
+package hr.yeti.rudimentary.autoregister.maven.plugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class RegisterAsServiceProvider extends SimpleFileVisitor<Path> {
+
+  private static final String HTTP_ENDPOINT_PROVIDERS = "hr.yeti.rudimentary.http.spi.HttpEndpoint";
+  private static final String VIEW_ENDPOINT_PROVIDERS = "hr.yeti.rudimentary.mvc.spi.ViewEndpoint";
+  private static final String INSTANCE_PROVIDERS = "hr.yeti.rudimentary.context.spi.Instance";
+
+  private Path projectRootDir;
+  private Path servicesDir;
+
+  private String httpEndpointProvidersList;
+  private String viewEndpointProvidersList;
+  private String instanceProvidersList;
+
+  public RegisterAsServiceProvider() {
+    try {
+      this.projectRootDir = new File("").toPath().toAbsolutePath();
+      this.servicesDir = Path.of(projectRootDir.toString(), "/src/main/resources/META-INF/services");
+
+      this.httpEndpointProvidersList = readProviders(servicesDir.resolve(HTTP_ENDPOINT_PROVIDERS));
+      this.viewEndpointProvidersList = readProviders(servicesDir.resolve(VIEW_ENDPOINT_PROVIDERS));
+      this.instanceProvidersList = readProviders(servicesDir.resolve(INSTANCE_PROVIDERS));
+
+    } catch (IOException ex) {
+      Logger.getLogger(RegisterAsServiceProvider.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  @Override
+  public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+    if (file.toString().endsWith(".java") && !file.toString().contains("module-info.java")) {
+
+      byte[] readAllBytes = Files.readAllBytes(file);
+      String content = new String(readAllBytes);
+
+      if (content.contains("implements HttpEndpoint<") && !content.contains("abstract")) {
+        writeProvider(HTTP_ENDPOINT_PROVIDERS, file);
+      } else if (content.contains("implements ViewEndpoint<") && !content.contains("abstract")) {
+        writeProvider(VIEW_ENDPOINT_PROVIDERS, file);
+      } else if (content.contains("implements Instance") && !content.contains("abstract")) {
+        writeProvider(INSTANCE_PROVIDERS, file);
+      }
+    }
+    return FileVisitResult.CONTINUE;
+  }
+
+  private String readProviders(Path path) throws IOException {
+    if (!Files.exists(path)) {
+      Files.createFile(path);
+    }
+    return Files.readString(path);
+  }
+
+  private String extractProvider(Path file) {
+    String[] parts = file.toAbsolutePath().toString().split("src/main/java");
+    return parts[1].replaceAll("/", ".").replace(".java", "").substring(1);
+  }
+
+  private void writeProvider(String providerType, Path file) throws IOException {
+    Path providers = servicesDir.resolve(providerType);
+    String provider = extractProvider(file);
+
+    boolean alreadyRegistered = false;
+
+    switch (providerType) {
+      case HTTP_ENDPOINT_PROVIDERS:
+        alreadyRegistered = httpEndpointProvidersList.contains(provider);
+        break;
+      case VIEW_ENDPOINT_PROVIDERS:
+        alreadyRegistered = viewEndpointProvidersList.contains(provider);
+        break;
+      case INSTANCE_PROVIDERS:
+        alreadyRegistered = instanceProvidersList.contains(provider);
+        break;
+      default:
+        break;
+    }
+
+    if (!alreadyRegistered) {
+      Files.write(providers, (provider + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
+    }
+
+  }
+}
