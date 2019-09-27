1314aa7a61b8dc73b9a10c04038eee29783a14de
vsmid <vedransmid@yeti-it.hr>
2019-09-27 16:45:49 +0200

    Watcher minor polishing.

--- rudimentary-cli/src/main/java/hr/yeti/rudimentary/cli/Watcher.java
+++ rudimentary-cli/src/main/java/hr/yeti/rudimentary/cli/Watcher.java
@@ -17,14 +17,10 @@ public class Watcher {
 
   private RunCommand cmd;
 
-  @SuppressWarnings("unchecked")
   static <T> WatchEvent<T> cast(WatchEvent<?> event) {
     return (WatchEvent<T>) event;
   }
 
-  /**
-   * Register the given directory with the WatchService
-   */
   private void register(Path dir) throws IOException {
     WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
     if (trace) {
@@ -40,11 +36,7 @@ public class Watcher {
     keys.put(key, dir);
   }
 
-  /**
-   * Register the given directory, and all its sub-directories, with the WatchService.
-   */
   private void registerAll(final Path start) throws IOException {
-    // register directory and sub-directories
     Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
       @Override
       public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
@@ -58,9 +50,6 @@ public class Watcher {
     });
   }
 
-  /**
-   * Creates a WatchService and registers the given directory
-   */
   public Watcher(Path dir, boolean recursive, RunCommand cmd) throws IOException {
     this.watcher = FileSystems.getDefault().newWatchService();
     this.keys = new HashMap<>();
@@ -68,24 +57,17 @@ public class Watcher {
     this.cmd = cmd;
 
     if (recursive) {
-      System.out.format("Scanning %s ...\n", dir);
       registerAll(dir);
-      System.out.println("Done.");
     } else {
       register(dir);
     }
 
-    // enable trace after initial registration
     this.trace = true;
   }
 
-  /**
-   * Process all events for keys queued to the watcher
-   */
   public void processEvents() throws IOException {
     for (;;) {
 
-      // wait for key to be signalled
       WatchKey key;
       try {
         key = watcher.take();
@@ -103,50 +85,41 @@ public class Watcher {
       for (WatchEvent<?> event : key.pollEvents()) {
         WatchEvent.Kind kind = event.kind();
 
-        // TBD - provide example of how OVERFLOW event is handled
         if (kind == OVERFLOW) {
           continue;
         }
 
-        // Context for directory entry event is the file name of entry
         WatchEvent<Path> ev = cast(event);
         Path name = ev.context();
         Path child = dir.resolve(name);
 
-        // print out event
-        System.out.format("%s: %s\n", event.kind().name(), child);
-
-        // if directory is created, and watching recursively, then
-        // register it and its sub-directories
+        //System.out.format("%s: %s\n", event.kind().name(), child);
         if (recursive && (kind == ENTRY_CREATE)) {
           try {
             if (Files.isDirectory(child, NOFOLLOW_LINKS)) {
               registerAll(child);
             }
-          } catch (IOException x) {
-            // ignore to keep sample readbale
+          } catch (IOException e) {
+            // Noop.
           }
         }
         found = true;
       }
 
       if (found && cmd.pid != null) {
+        System.out.println("Changes detected...initiating reload....");
+        
         ProcessHandle.of(Long.valueOf(cmd.pid)).get().destroy();
         cmd.consoleReader.setStop(true);
         cmd.pid = null;
 
-        found = false;
-
         cmd.mavenRunRudyApplication();
         cmd.readProcessStdOut();
       }
-
-      // reset key and remove from set if directory no longer accessible
       boolean valid = key.reset();
       if (!valid) {
         keys.remove(key);
 
-        // all directories are inaccessible
         if (keys.isEmpty()) {
           break;
         }
