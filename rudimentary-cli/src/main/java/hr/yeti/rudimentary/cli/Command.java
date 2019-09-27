d408857a89481e15f7e296423a7b904f7382b14d
vsmid <vedransmid@yeti-it.hr>
2019-09-27 16:45:05 +0200

    Removed cygwin mingw method.

--- rudimentary-cli/src/main/java/hr/yeti/rudimentary/cli/Command.java
+++ rudimentary-cli/src/main/java/hr/yeti/rudimentary/cli/Command.java
@@ -1,6 +1,5 @@
 package hr.yeti.rudimentary.cli;
 
-import java.io.IOException;
 import java.util.Map;
 
 public interface Command {
@@ -17,27 +16,4 @@ public interface Command {
     return System.getProperty("os.name").toLowerCase().contains("windows");
   }
 
-  default boolean isCygwinOrMingw() {
-    boolean cygwinMingw = false;
-
-    Process unameProcess = null;
-    try {
-      unameProcess = new ProcessBuilder("uname", "-s").start();
-
-      StringBuilder consoleOutput = new StringBuilder();
-      int read = 0;
-
-      while ((read = unameProcess.getInputStream().read()) != -1) {
-        consoleOutput.append((char) read);
-      }
-
-      if (consoleOutput.toString().toLowerCase().contains("mingw") || consoleOutput.toString().toLowerCase().contains("cygwin")) {
-        cygwinMingw = true;
-      }
-    } catch (IOException ex) {
-      // Noop.
-    }
-
-    return cygwinMingw;
-  }
 }
