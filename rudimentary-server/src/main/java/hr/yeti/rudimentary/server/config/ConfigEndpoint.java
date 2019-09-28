465746102c8b953becd136febae8a0b11cd0ea7f
Vedran Smid <vedransmid@yeti-it.hr>
2019-09-28 11:54:06 +0200

    Added basic _config endpoint.

---
+++ rudimentary-server/src/main/java/hr/yeti/rudimentary/server/config/ConfigEndpoint.java
@@ -0,0 +1,77 @@
+package hr.yeti.rudimentary.server.config;
+
+import hr.yeti.rudimentary.config.spi.Config;
+import hr.yeti.rudimentary.http.HttpMethod;
+import hr.yeti.rudimentary.http.Request;
+import hr.yeti.rudimentary.http.content.Empty;
+import hr.yeti.rudimentary.http.content.Html;
+import hr.yeti.rudimentary.http.spi.HttpEndpoint;
+import hr.yeti.rudimentary.server.resources.ClasspathResource;
+import java.io.IOException;
+import java.lang.System.Logger.Level;
+import java.net.URI;
+import java.util.stream.Collectors;
+
+public class ConfigEndpoint implements HttpEndpoint<Empty, Html> {
+
+  private String configHTML;
+
+  @Override
+  public void initialize() {
+    try {
+      this.configHTML = new String(
+          new ClasspathResource("/templates/config.html")
+              .get()
+              .readAllBytes()
+      );
+    } catch (IOException ex) {
+      logger().log(Level.ERROR, "Failed to load config.html template.", ex);
+    }
+  }
+
+  @Override
+  public HttpMethod httpMethod() {
+    return HttpMethod.GET;
+  }
+
+  @Override
+  public URI path() {
+    return URI.create("/_config");
+  }
+
+  @Override
+  public int httpStatus() {
+    return 200;
+  }
+
+  @Override
+  public Html response(Request<Empty> request) {
+    return new Html(
+        String.format(configHTML,
+            rowsHTMLGenerator()
+        )
+    );
+  }
+
+  @Override
+  public String description() {
+    return "Lists current application configuration properties.";
+  }
+
+  private String rowsHTMLGenerator() {
+    String rowsHTML = Config.provider().applicationProperties()
+        .entrySet()
+        .stream()
+        .map(e
+            -> String.format("<tr><td>%s</td><td>%s</td></tr>",
+            e.getKey(),
+            e.getValue())
+        )
+        .collect(
+            Collectors.joining(System.lineSeparator())
+        );
+
+    return rowsHTML;
+  }
+
+}
