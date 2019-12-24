7af603ca94bd92ea2465fad9cbd1efdad42bbc39
Vedran Smid <vedransmid@yeti-it.hr>
2019-12-24 09:39:39 +0100

    Refactored CustomViewEndpoint.

--- rudimentary-demo/src/main/java/hr/yeti/rudimentary/demo/endpoint/CustomViewEndpoint.java
+++ rudimentary-demo/src/main/java/hr/yeti/rudimentary/demo/endpoint/CustomViewEndpoint.java
@@ -1,12 +1,16 @@
 package hr.yeti.rudimentary.demo.endpoint;
 
+import com.sun.net.httpserver.Headers;
 import hr.yeti.rudimentary.http.Request;
-import hr.yeti.rudimentary.http.content.Form;
+import hr.yeti.rudimentary.http.content.Empty;
 import hr.yeti.rudimentary.http.content.View;
 import hr.yeti.rudimentary.mvc.spi.ViewEndpoint;
+import static hr.yeti.rudimentary.validation.Constraint.NOT_NULL;
+import hr.yeti.rudimentary.validation.Constraints;
 import java.net.URI;
+import java.util.Map;
 
-public class CustomViewEndpoint implements ViewEndpoint<Form> {
+public class CustomViewEndpoint implements ViewEndpoint<Empty> {
 
     @Override
     public URI path() {
@@ -14,8 +18,18 @@ public class CustomViewEndpoint implements ViewEndpoint<Form> {
     }
 
     @Override
-    public View response(Request<Form> request) {
-        return new View("dynamicView.html", request.getBody().getValue());
+    public Constraints constraints(Empty body, Map<String, String> pathVariables, Map<String, String> queryParameters, Headers httpHeaders) {
+        return new Constraints() {
+            {
+                o(queryParameters.get("name"), NOT_NULL);
+                o(queryParameters.get("age"), NOT_NULL);
+            }
+        };
+    }
+
+    @Override
+    public View response(Request<Empty> request) {
+        return new View("dynamicView.html", request.getQueryParameters());
     }
 
 }
