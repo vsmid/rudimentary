0b246ab0d0d66ef8f73cb32a6aa635bc7da8c137
Vedran Smid <vedransmid@yeti-it.hr>
2020-01-02 10:04:52 +0100

    Added apidocs endpoint test.

---
+++ rudimentary-server/src/test/java/hr/yeti/rudimentary/server/apidocs/ApiDocsEndpointTest.java
@@ -0,0 +1,49 @@
+package hr.yeti.rudimentary.server.apidocs;
+
+import hr.yeti.rudimentary.http.MediaType;
+import hr.yeti.rudimentary.server.test.TestServer;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ApiDocsEndpointTest {
+
+    static TestServer testServer;
+
+    @BeforeAll
+    public static void beforeAll() {
+        testServer = TestServer.newBuilder()
+            .httpEndpoints(ApiDocsEndpoint.class)
+            .build();
+        testServer.start();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        testServer.stop();
+    }
+
+    @Test
+    public void test_apidocs_response() throws IOException, InterruptedException {
+        // setup:
+        URI uri = testServer.buildUri("_apidocs");
+        HttpRequest GET = HttpRequest.newBuilder(uri).build();
+        HttpResponse<String> response;
+
+        when:
+        response = HttpClient.newHttpClient().send(GET, HttpResponse.BodyHandlers.ofString());
+
+        then:
+        assertEquals(200, response.statusCode());
+        assertEquals(MediaType.TEXT_HTML, response.headers().firstValue("content-type").get());
+        assertTrue(response.body().contains("<tr><td>GET</td><td>/_apidocs</td><td>200</td><td>Shows API documentation in HTML format.</td></tr>"));
+    }
+
+}
