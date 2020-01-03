377cf41f88a453843c697ef0f40aaf86f5c854c6
vsmid <vedransmid@yeti-it.hr>
2020-01-03 16:04:14 +0100

    Identity collections are now unmodifyable.

--- rudimentary-api/src/main/java/hr/yeti/rudimentary/security/Identity.java
+++ rudimentary-api/src/main/java/hr/yeti/rudimentary/security/Identity.java
@@ -1,32 +1,34 @@
 package hr.yeti.rudimentary.security;
 
 import com.sun.net.httpserver.HttpPrincipal;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Identity<I> extends HttpPrincipal {
 
     private String password;
-    private List<String> groups = new ArrayList<>();
-    private List<String> roles = new ArrayList<>();
+    private List<String> groups;
+    private List<String> roles;
     private I details;
 
     public Identity(Identity identity, I details) {
         super(identity.getUsername(), identity.getPassword());
-        this.groups.addAll(identity.getGroups());
-        this.roles.addAll(identity.getRoles());
+        this.groups = Collections.unmodifiableList(identity.getGroups());
+        this.roles = Collections.unmodifiableList(identity.getRoles());
         this.details = details;
     }
 
     public Identity(String username, String realm) {
         super(username, realm);
+        this.groups = List.of();
+        this.roles = List.of();
     }
 
     public Identity(List<String> groups, List<String> roles, I details, String username, String password, String realm) {
         super(username, realm);
         this.password = password;
-        this.groups.addAll(groups);
-        this.roles.addAll(roles);
+        this.groups = Collections.unmodifiableList(groups);
+        this.roles = Collections.unmodifiableList(roles);
         this.details = details;
     }
 
