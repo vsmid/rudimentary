8adb73c3cbfc0c4ef1d741e36e3267dad6e17b43
Vedran Smid <vedransmid@yeti-it.hr>
2020-01-04 09:33:00 +0100

    BasicDataSource can now also be configured with dataSource.properties.*.

--- rudimentary-api/src/main/java/hr/yeti/rudimentary/sql/spi/BasicDataSource.java
+++ rudimentary-api/src/main/java/hr/yeti/rudimentary/sql/spi/BasicDataSource.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
 import java.sql.ShardingKeyBuilder;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.logging.Logger;
 import javax.sql.DataSource;
 
@@ -19,16 +20,14 @@ import javax.sql.DataSource;
  * Class to extends when you wish to add another datasource.
  *
  * <p>
- * Since this abstract class implements {@link Instance} it means it is loaded automatically via {@link ServiceLoader}
- * on application startup. Default datasource used by Rudimentary is registered in <i><rudimentary-server</i> module in
- * file <i>src/main/resources/META-INF/services/hr.yeti.rudimentary.sql.spi.BasicDataSource</i>.
+ * Since this abstract class implements {@link Instance} it means it is loaded automatically via {@link ServiceLoader} on application startup. Default datasource used by Rudimentary is registered in
+ * <i><rudimentary-server</i> module in file <i>src/main/resources/META-INF/services/hr.yeti.rudimentary.sql.spi.BasicDataSource</i>.
  * </p>
  *
- * When you add another datasource the main thing to do is to override {@link Instance#id()} method and set it to unused
- * value. That value used must also be used in datasource configuration found in configuration properties.
+ * When you add another datasource the main thing to do is to override {@link Instance#id()} method and set it to unused value. That value used must also be used in datasource configuration found in
+ * configuration properties.
  *
- * For example, if we set {@link Instance#id()} to value myNewDatasource then all datasource configuration properties
- * should look something like this:
+ * For example, if we set {@link Instance#id()} to value myNewDatasource then all datasource configuration properties should look something like this:
  *
  * <pre>
  * dataSource.myNewDatasource.enabled=false
@@ -36,10 +35,13 @@ import javax.sql.DataSource;
  * dataSource.myNewDatasource.jdbcUrl=
  * dataSource.myNewDatasource.username=
  * dataSource.myNewDatasource.password=
+ *
+ * # Or if used with properties.*
+ * dataSource.myNewDatasource.properties.dataSource.username=
  * ...
  * </pre>
  *
- *
+ * @see https://github.com/brettwooldridge/HikariCP for how to use and configure HikariCP.
  * @author vedransmid@yeti-it.hr
  */
 public abstract class BasicDataSource implements DataSource, Instance {
@@ -68,20 +70,41 @@ public abstract class BasicDataSource implements DataSource, Instance {
 
     public DataSource dataSource() {
         if (Config.provider().property(propertyName("enabled")).asBoolean()) {
-            if (Config.provider().contains(propertyName("jdbcUrl"))) {
-                HikariConfig config = new HikariConfig();
+
+            Properties dataSourceProperties = Config.provider()
+                .getPropertiesByPrefix(
+                    propertyName("properties"),
+                    false
+                );
+
+            HikariConfig config = new HikariConfig(dataSourceProperties);
+
+            if (Config.provider().containsNotBlank(propertyName("jdbcUrl"), true)) {
                 config.setJdbcUrl(Config.provider().property(propertyName("jdbcUrl")).value());
+            }
+
+            if (Config.provider().containsNotBlank(propertyName("driverClassName"), true)) {
                 config.setDriverClassName(Config.provider().property(
                     propertyName("driverClassName")
                 ).value());
+            }
+
+            if (Config.provider().containsNotBlank(propertyName("username"), true)) {
                 config.setUsername(Config.provider().property(propertyName("username")).value());
+            }
+
+            if (Config.provider().containsNotBlank(propertyName("password"), true)) {
                 config.setPassword(Config.provider().property(propertyName("password")).value());
+            }
+
+            if (Config.provider().containsNotBlank(propertyName("maximumPoolSize"), true)) {
                 config.setMaximumPoolSize(Config.provider().property(
                     propertyName("maximumPoolSize")).asInt()
                 );
-
-                return new HikariDataSource(config);
             }
+
+            return new HikariDataSource(config);
+
         }
         return null;
     }
