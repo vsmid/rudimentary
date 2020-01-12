c285ade59cd23e71d59ad5bea61c4054d5548d4b
Vedran Smid <vedransmid@yeti-it.hr>
2020-01-12 13:21:49 +0100

    Fixed Cookie construction.

--- rudimentary-api/src/main/java/hr/yeti/rudimentary/http/Cookie.java
+++ rudimentary-api/src/main/java/hr/yeti/rudimentary/http/Cookie.java
@@ -1,6 +1,10 @@
 package hr.yeti.rudimentary.http;
 
 import java.net.HttpCookie;
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class Cookie {
@@ -14,17 +18,34 @@ public class Cookie {
 
     @Override
     public String toString() {
-        return pair(httpCookie.getName(), httpCookie.getValue())
-            + pair("Path", httpCookie.getPath())
-            + pair("Expires", httpCookie.getMaxAge())
-            + pair("Domain", httpCookie.getDomain())
-            + pair("sameSite", sameSiteStrict ? "strict" : "lax")
-            + (httpCookie.isHttpOnly() ? " HttpOnly;" : "")
-            + (httpCookie.getSecure() ? " Secured;" : "");
+        StringBuilder cookieString = new StringBuilder();
+
+        cookieString
+            .append(pair(httpCookie.getName(), httpCookie.getValue()))
+            .append(pair("Path", httpCookie.getPath()))
+            .append(pair("Domain", httpCookie.getDomain()))
+            .append(pair("SameSite", sameSiteStrict ? "strict" : "lax"));
+
+        if (httpCookie.getMaxAge() > -1) {
+            OffsetDateTime expires = OffsetDateTime.now(ZoneOffset.UTC).plus(Duration.ofSeconds(httpCookie.getMaxAge()));
+            cookieString.append(
+                pair("Expires", DateTimeFormatter.RFC_1123_DATE_TIME.format(expires))
+            );
+        }
+
+        if (httpCookie.isHttpOnly()) {
+            cookieString.append("HttpOnly;");
+        }
+
+        if (httpCookie.getSecure()) {
+            cookieString.append("Secured;");
+        }
+
+        return cookieString.toString();
     }
 
     private String pair(String name, Object value) {
-        return String.format("%s=%s; ", name, Objects.isNull(value) ? "" : value.toString());
+        return String.format("%s=%s;", name, Objects.isNull(value) ? "" : value.toString());
     }
 
     public void setSameSiteStrict(boolean strict) {
