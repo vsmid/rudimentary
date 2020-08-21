6a05e7d67febdb20288c87ef8262f93fb49b72c4
Vedran Smid <vedransmid@gmail.com>
2020-08-21 14:05:35 +0200

    Added application/xml to MediaType.

--- rudimentary-api/src/main/java/hr/yeti/rudimentary/http/MediaType.java
+++ rudimentary-api/src/main/java/hr/yeti/rudimentary/http/MediaType.java
@@ -11,6 +11,7 @@ public class MediaType {
     public static final String ALL = "*/*";
     public static final String APPLICATION_JAVASCRIPT = "application/javascript";
     public static final String APPLICATION_JSON = "application/json";
+    public static final String APPLICATION_XML = "application/xml";
     public static final String APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded";
     public static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
     public static final String TEXT_HTML = "text/html";
