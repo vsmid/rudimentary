10fab7d3fd92b49963f41784f5a27b95eba508c8
Vedran Smid <vedransmid@gmail.com>
2020-08-22 08:38:58 +0200

    Preparing tests for xml content handler.

---
+++ rudimentary-exts/rudimentary-http-xml-ext/src/test/java/hr/yeti/rudimentary/exts/http/xml/TestHttpExchangeImpl.java
@@ -0,0 +1,122 @@
+package hr.yeti.rudimentary.exts.http.xml;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpPrincipal;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+
+public class TestHttpExchangeImpl extends HttpExchange {
+
+    private InputStream is;
+    private OutputStream os;
+    private Headers reqHeaders = new Headers();
+    private Headers respHeaders = new Headers();
+    private int httpStatus;
+
+    public TestHttpExchangeImpl() {
+    }
+
+    public TestHttpExchangeImpl(InputStream is) {
+        this.is = is;
+    }
+
+    @Override
+    public Headers getRequestHeaders() {
+        return reqHeaders;
+    }
+
+    @Override
+    public Headers getResponseHeaders() {
+        return respHeaders;
+    }
+
+    @Override
+    public URI getRequestURI() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getRequestMethod() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HttpContext getHttpContext() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void close() {
+        try {
+            os.flush();
+            os.close();
+        } catch (IOException ex) {
+            System.getLogger(TestHttpExchangeImpl.class.getName()).log(System.Logger.Level.ERROR, ex);
+        }
+    }
+
+    @Override
+    public InputStream getRequestBody() {
+        return is;
+    }
+
+    @Override
+    public OutputStream getResponseBody() {
+        return os;
+    }
+
+    @Override
+    public void sendResponseHeaders(int rCode, long responseLength) throws IOException {
+        this.httpStatus = rCode;
+        if (responseLength != -1) {
+            this.os = new ByteArrayOutputStream((int) responseLength);
+        }
+    }
+
+    @Override
+    public InetSocketAddress getRemoteAddress() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getResponseCode() {
+        return this.httpStatus;
+    }
+
+    @Override
+    public InetSocketAddress getLocalAddress() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getProtocol() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setAttribute(String name, Object value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setStreams(InputStream i, OutputStream o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HttpPrincipal getPrincipal() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ rudimentary-exts/rudimentary-http-xml-ext/src/test/java/hr/yeti/rudimentary/exts/http/xml/XmlPojoContentHandlerTest.java
@@ -0,0 +1,106 @@
+package hr.yeti.rudimentary.exts.http.xml;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.sun.net.httpserver.HttpExchange;
+import hr.yeti.rudimentary.http.MediaType;
+import hr.yeti.rudimentary.http.Request;
+import hr.yeti.rudimentary.http.content.Pojo;
+import hr.yeti.rudimentary.http.content.handler.spi.ContentHandler;
+import hr.yeti.rudimentary.http.spi.HttpEndpoint;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+public class XmlPojoContentHandlerTest {
+
+    static ContentHandler<Pojo> contentHandler;
+
+    @BeforeAll
+    public static void beforeAll() {
+        contentHandler = new XmlPojoContentHandler();
+    }
+
+    @Test
+    public void test_read_activate() {
+        // setup:
+        HttpExchange httpExchange = new TestHttpExchangeImpl();
+
+        when:
+        httpExchange.getRequestHeaders().add("Content-Type", MediaType.APPLICATION_XML);
+
+        then:
+        assertTrue(contentHandler.activateReader(XmlPojoEndpoint.class, httpExchange));
+    }
+
+    @Test
+    public void test_write_activate() {
+        // setup:
+        HttpExchange httpExchange = new TestHttpExchangeImpl();
+
+        expect:
+        assertTrue(contentHandler.activateWriter(XmlPojoEndpoint.class, httpExchange));
+    }
+
+    @Test
+    public void test_read() throws IOException {
+        // setup:
+        HttpExchange httpExchange = new TestHttpExchangeImpl(new ByteArrayInputStream("<inOut><name>Lena</name><inOut/>".getBytes()));
+        InOut inOut;
+
+        when:
+        inOut = (InOut) contentHandler.read(httpExchange, XmlPojoEndpoint.class);
+
+        then:
+        assertEquals("Lena", inOut.getName());
+    }
+
+    @Test
+    public void test_write() throws IOException {
+        // setup:
+        HttpExchange httpExchange = new TestHttpExchangeImpl();
+
+        when:
+        contentHandler.write(200, new InOut("Lena"), httpExchange, XmlPojoEndpoint.class);
+
+        then:
+        assertEquals(httpExchange.getResponseCode(), 200);
+        assertEquals(MediaType.APPLICATION_XML, httpExchange.getResponseHeaders().get("Content-Type").get(0));
+        assertEquals("<inOut><name>Lena</name><inOut/>", new String(((ByteArrayOutputStream) httpExchange.getResponseBody()).toByteArray()));
+    }
+
+    public static class XmlPojoEndpoint implements HttpEndpoint<InOut, InOut> {
+
+        @Override
+        public InOut response(Request<InOut> request) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+    }
+
+    @XmlRootElement
+    public static class InOut extends Pojo {
+
+        private String name;
+
+        public InOut() {
+        }
+
+        public InOut(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+
+}
